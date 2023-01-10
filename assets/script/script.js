5d5f2ca32990a3ae836893c7295a97ce9a69e7a4
skwidhubz <tommaxwellparker@gmail.com>
2023-01-10 14:26:37 +1100

    file structure and pseudo coding js

---
+++ assets/script/script.js
@@ -0,0 +1,5 @@
+// fetching API weather data
+// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+// APIKEY: dec4130e8e70f58568a8b5e353c2ec7b
+// getGeo: 
+GeolocationCoordinates
