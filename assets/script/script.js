3aff9c328cc8236d44a26a439d108aa89ddccb89
skwidhubz <tommaxwellparker@gmail.com>
2023-01-19 11:08:18 +1100

    api call error

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,42 +1,29 @@
-// fetching API weather data
-// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
-// APIKEY: b5a40f59d7b1cf68e5036b045244a798
-// getGeo: 
-// GeolocationCoordinates
+// function getLatLong(){
+//     console.log("button works");
+//     navigator.geolocation.getCurrentPosition((position) => {
+//     const lat = position.coords.latitude;
+//     const long = position.coords.longitude;
+//     console.log(lat);
+//     console.log(long);
+//     var queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+long+"&appid=b5a40f59d7b1cf68e5036b045244a798"
+//     return queryURL
+// });
+// }
 
-// var geoLocLat = GeolocationCoordinates.latitude
-// var geoLocLon = GeolocationCoordinates.longitude
-
-
-
-
-
-function getLatLong(){
-    console.log("button works");
-    navigator.geolocation.getCurrentPosition((position) => {
-    const lat = position.coords.latitude;
-    const long = position.coords.longitude;
-    console.log(lat);
-    console.log(long);
-    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+long+"&appid=b5a40f59d7b1cf68e5036b045244a798"
-    return queryURL
-});
-}
-
-getLatLong();
+// getLatLong();
 
 
+var submitBtn = $("#submit-btn");
 
+submitBtn.click(getCurrentWeather);
 
-var weatherBtn = $("#weather-btn");
+var cityName = $("#city-name")
+var cityNameURL = cityName.value
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+cityNameURL+"&appid=b5a40f59d7b1cf68e5036b045244a798"
 
-weatherBtn.click(getCurrentWeather);
-
-function getCurrentWeather(queryURL){
+function getCurrentWeather(){
     fetch(queryURL)
     .then((response) => response.json())
     .then((data) => console.log(data));
+  
 }
-
-// getCurrentWeather()
-
